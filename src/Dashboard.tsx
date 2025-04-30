77f9767270637c40f3d69545358bc320f7b63534
segevitsky@gmail.com <segevitsky@gmail.com>
2025-04-30 12:17:55 +0300

    remove unneeded state for vercel

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -31,7 +31,7 @@ const Dashboard = () => {
   
   
   const [jiraSaveMessage, setJiraMessage] = useState({ type: '', text: '' });
-  const [slackSaveMessage, setSlackMessage] = useState({ type: '', text: '' });
+  // const [slackSaveMessage, setSlackMessage] = useState({ type: '', text: '' });
 
   
 
@@ -249,7 +249,7 @@ const Dashboard = () => {
           <div className="flex items-center justify-between mb-6">
             <h3 className="text-xl font-bold">Allowed Domains</h3>
             <button
-              
+
               className="px-4 py-2 bg-gradient-to-r from-pink-500 to-rose-500 text-white rounded-lg hover:opacity-90 transition-opacity">
               Add Domain
             </button>  
